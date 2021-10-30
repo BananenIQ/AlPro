7b89fd6ca6bcdb259a3a62e76743e5596640928f
Krystian Przywozny <krystianprzywozny@gmail.com>
2021-10-30 14:24:58 +0200

    update (+ggt, +kgV

--- C 02/aufgabe3.c
+++ C 02/aufgabe3.c
@@ -14,32 +14,48 @@
 
 /* Hier kommt euer Programmcode hin */
 
-// 
-// 
-int ggT(int x, int y){
-    int rest;
-    if (x == 0) return abs(y);
-    if (y == 0) return abs(x);
-    do {
-        rest = x % y;
-        x = y;
-        y = rest;
-    } while (y != 0);
+int ggT(int x, int y) {    /*Diese Funktion sucht den ggT der beiden eingegeben Ganzzahlen X und Y*/
+    x = abs(x);
+    y = abs(y);
 
-    return abs(x);
+    if (x < y) {             /* If-Abfrage ob x kleiner als y ist*/
+        int cache = x;       /*Falls ja werden die werte in X und Y getauscht*/
+        x = y;              
+        y = cache;
+    }
+
+    for (int i = 1; i <= y; i++) {            /*For-Schleife die in 1er Schritten von 1 bis Y durchiteriert*/
+        if (y % i == 0 && x % (y / i) == 0) { /*Die erste Bedingung ermittelt den nächsten absteigenden Teiler der kleineren Zahl, die Zweite überprüft ob dieser Teiler auch die größere Zahl teilt*/
+            return y / i;                     /*Falls beide Bedingungen erfüllt sind wird der aktuelle Teiler zurückgegeben*/
+        }
+    }
+    return -1;
 }
 
-int kgV(int x, int y){
+int kgV(int x, int y) {  /*Diese Funktion sucht den kgV der beiden eingegeben Ganzzahlen X und Y*/
+    x = abs(x);
+    y = abs(y);
+    
+    if (x < y) {             /* If-Abfrage ob x kleiner als y ist*/
+        int cache = x;       /*Falls ja werden die werte in X und Y getauscht*/
+        x = y;              
+        y = cache;
+    }
 
-    return 24;
+    for (int i = 1; i <= y; i++) {     /*For-Schleife die in 1er Schritten von 1 bis Y durchiteriert*/
+        if ((i * x) % y == 0) {        /*Falls ein Vielfaches von x durch y teilbar ist wird dieses kgVielfaches ausgegeben*/
+            return i * x;
+        }
+    }
+    return -1;
 }
 
 int main() {
-  // Lies x ein (ein bisschen Magie)
-  char buf[0x30];
-  int x = atoi(fgets(buf, 0x30, stdin)); // Lese x ein
-  int y = atoi(fgets(buf, 0x30, stdin)); // Lese y ein
-  
-  printf("ggT(x,y) = %d, kgV(x,y) = %d\n", ggT(x,y), kgV(x,y)); // Rufe Funktion ggT und kgV auf und printe den Wert von ggT(x,y), kgV(x,y) 
-  return 0;
+    // Lies x ein (ein bisschen Magie)
+    char buf[0x30];
+    int x = atoi(fgets(buf, 0x30, stdin)); // Lese x ein
+    int y = atoi(fgets(buf, 0x30, stdin)); // Lese y ein
+
+    printf("ggT(x,y) = %d, kgV(x,y) = %d\n", ggT(x, y), kgV(x, y)); // Rufe Funktion ggT und kgV auf und printe den Wert von ggT(x,y), kgV(x,y) 
+    return 0;
 }
